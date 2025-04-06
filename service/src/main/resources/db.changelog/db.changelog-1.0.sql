43c1c0e3bd540ebdb0447b28bd42669ed284a055
Naivezz <vykpishdima1@gmail.com>
2025-04-07 00:40:31 +0200

    Added liquibase to my project : script and master file

---
+++ service/src/main/resources/db.changelog/db.changelog-1.0.sql
@@ -0,0 +1,75 @@
+--liquibase formatted sql
+
+--changeset naivez:1
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    firstname VARCHAR(128) NOT NULL,
+    lastname  VARCHAR(128) NOT NULL,
+    email VARCHAR(128) UNIQUE NOT NULL,
+    password VARCHAR(128) NOT NULL,
+    phone_number VARCHAR(64) NOT NULL,
+    role VARCHAR(32) NOT NULL,
+    is_blacklisted BOOLEAN DEFAULT FALSE,
+    blacklist_reason TEXT
+);
+
+--changeset naivez:2
+CREATE TABLE restaurant (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    address VARCHAR(256) NOT NULL,
+    image VARCHAR(128),
+    phone_number VARCHAR(64) NOT NULL
+);
+
+--changeset naivez:3
+CREATE TABLE reservation (
+    id BIGSERIAL PRIMARY KEY,
+    time TIMESTAMP NOT NULL,
+    guests INTEGER NOT NULL,
+    status VARCHAR(32) NOT NULL,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurant(id) ON DELETE CASCADE,
+    amount DECIMAL(10, 2),
+    payment_time TIMESTAMP,
+    payment_status VARCHAR(32)
+);
+
+--changeset naivez:4
+CREATE TABLE menu_item (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    image VARCHAR(128),
+    description TEXT,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurant(id) ON DELETE CASCADE
+);
+
+--changeset naivez:5
+CREATE TABLE reservation_menu_item (
+    id BIGSERIAL PRIMARY KEY,
+    reservation_id BIGINT NOT NULL REFERENCES reservation(id) ON DELETE CASCADE,
+    menu_item_id INT NOT NULL REFERENCES menu_item(id),
+    quantity INT NOT NULL
+);
+
+--changeset naivez:6
+CREATE TABLE review (
+    id SERIAL PRIMARY KEY,
+    rating INTEGER CHECK (rating BETWEEN 1 AND 5),
+    description TEXT,
+    time TIMESTAMP NOT NULL,
+    image VARCHAR(128),
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurant(id) ON DELETE CASCADE
+);
+
+
+--changeset naivez:7
+CREATE TABLE event (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    time TIMESTAMP NOT NULL,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurant(id) ON DELETE CASCADE
+);
