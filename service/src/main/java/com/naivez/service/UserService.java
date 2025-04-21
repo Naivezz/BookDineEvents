d02b3908a7e6678565f8dd441a31fcb84095dfd0
Naivezz <vykpishdima1@gmail.com>
2025-04-21 16:19:20 +0200

    Updated logic with user password

--- service/src/main/java/com/naivez/service/UserService.java
+++ service/src/main/java/com/naivez/service/UserService.java
@@ -1,10 +1,12 @@
 package com.naivez.service;
 
 import com.naivez.dto.user.UserCreateEditDto;
+import com.naivez.dto.user.UserFilter;
 import com.naivez.dto.user.UserReadDto;
 import com.naivez.mapper.user.UserCreateEditMapper;
 import com.naivez.mapper.user.UserReadMapper;
 import com.naivez.repository.UserRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +23,12 @@ public class UserService {
     private final UserReadMapper userReadMapper;
     private final UserCreateEditMapper userCreateEditMapper;
 
+    public List<UserReadDto> findAll(UserFilter filter) {
+        return userRepository.findAllByFilter(filter).stream()
+                .map(userReadMapper::toDto)
+                .toList();
+    }
+
     public List<UserReadDto> findAll() {
         return userRepository.findAll().stream()
                 .map(userReadMapper::toDto)
@@ -33,7 +41,7 @@ public class UserService {
     }
 
     @Transactional
-    public UserReadDto create(UserCreateEditDto userCreateEditDto) {
+    public UserReadDto create(@Valid UserCreateEditDto userCreateEditDto) {
         return Optional.of(userCreateEditDto)
                 .map(userCreateEditMapper::toEntity)
                 .map(userRepository::save)
@@ -42,9 +50,12 @@ public class UserService {
     }
 
     @Transactional
-    public Optional<UserReadDto> update(Long id, UserCreateEditDto userCreateEditDto) {
+    public Optional<UserReadDto> update(Long id, @Valid UserCreateEditDto userCreateEditDto) {
         return userRepository.findById(id)
                 .map(entity -> {
+                    if (userCreateEditDto.getPassword() == null || userCreateEditDto.getPassword().isEmpty()) {
+                        userCreateEditDto.setPassword(entity.getPassword());
+                    }
                     userCreateEditMapper.toEntity(userCreateEditDto, entity);
                     return entity;
                 })
