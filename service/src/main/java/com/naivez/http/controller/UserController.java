97382dc8c4f120f2d585a60cce59928682f2bc91
Naivezz <vykpishdima1@gmail.com>
2025-04-21 16:19:52 +0200

    Added new endpoints - edit , registration

--- service/src/main/java/com/naivez/http/controller/UserController.java
+++ service/src/main/java/com/naivez/http/controller/UserController.java
@@ -1,18 +1,19 @@
-package com.naivez.controller;
+package com.naivez.http.controller;
 
 import com.naivez.dto.user.UserCreateEditDto;
+import com.naivez.dto.user.UserFilter;
 import com.naivez.entity.enums.Role;
 import com.naivez.service.UserService;
-import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 @Controller
@@ -22,9 +23,9 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping()
-    public String findAll(Model model){
-        model.addAttribute("users", userService.findAll());
+    @GetMapping
+    public String findAll(Model model, UserFilter filter) {
+        model.addAttribute("users", userService.findAll(filter));
         return "user/users";
     }
 
@@ -39,14 +40,31 @@ public class UserController {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @GetMapping("/registration")
+    public String registration(Model model, @ModelAttribute("user") @Valid UserCreateEditDto user) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "user/registration";
+    }
+
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public String create(@ModelAttribute UserCreateEditDto user) {
+    public String create(@ModelAttribute @Valid UserCreateEditDto user) {
         return "redirect:/users/" + userService.create(user).getId();
     }
 
+    @GetMapping("/{id}/update")
+    public String edit(@PathVariable("id") Long id, Model model) {
+        return userService.findById(id)
+                .map(user -> {
+                    model.addAttribute("user", user);
+                    model.addAttribute("roles", Role.values());
+                    return "user/edit";
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
     @PostMapping("/{id}/update")
-    public String update(@PathVariable Long id, @ModelAttribute UserCreateEditDto user) {
+    public String update(@PathVariable Long id, @ModelAttribute @Valid UserCreateEditDto user) {
         return userService.update(id, user)
                 .map(it -> "redirect:/users/{id}")
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
