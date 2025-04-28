7bf5d3d40bf93c04dfd8665eae72c30219cf8059
Naivezz <vykpishdima1@gmail.com>
2025-04-28 15:05:46 +0200

    Implement based query to find User by email

--- service/src/main/java/com/naivez/repository/UserRepository.java
+++ service/src/main/java/com/naivez/repository/UserRepository.java
@@ -4,5 +4,8 @@ import com.naivez.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long>, QuerydslPredicateExecutor<User>, FilterUserRepository{
+    Optional<User> findByEmail(String email);
 }
