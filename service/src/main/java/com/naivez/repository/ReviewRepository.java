8a88be7b95969755ea3b3ac7598dcfd67eed7242
Naivezz <vykpishdima1@gmail.com>
2025-05-05 18:47:15 +0200

    Extended RestaurantRepository interface to support filtering with QueryDSL and custom methods

--- service/src/main/java/com/naivez/repository/ReviewRepository.java
+++ service/src/main/java/com/naivez/repository/ReviewRepository.java
@@ -1,7 +1,9 @@
 package com.naivez.repository;
 
+import com.naivez.entity.Restaurant;
 import com.naivez.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long>, QuerydslPredicateExecutor<Review>, FilterReviewRepository {
 }
