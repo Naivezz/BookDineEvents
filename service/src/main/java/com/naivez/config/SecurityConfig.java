19dd6c6e3ed4cd40e7deefd45693541d70dfecc1
Naivezz <vykpishdima1@gmail.com>
2025-05-05 18:30:51 +0200

    Fixed security config , make easier config , moved the functionality to a separate class

--- service/src/main/java/com/naivez/config/SecurityConfig.java
+++ service/src/main/java/com/naivez/config/SecurityConfig.java
@@ -1,12 +1,10 @@
 package com.naivez.config;
 
-import com.naivez.entity.User;
-import com.naivez.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,36 +12,25 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
-    private final UserRepository userRepository;
+    private final CustomAuthenticationSuccessHandler successHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                                .requestMatchers("/login", "/users/registration", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
-                                .requestMatchers("/users").hasAuthority("ADMIN")
-                                .requestMatchers("/reservations/**").authenticated()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/users/registration", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/users").hasAuthority("ADMIN")
+                        .requestMatchers("/reservations/**").authenticated()
                         .anyRequest().authenticated()
                 )
-                .formLogin(formLogin -> formLogin
+                .formLogin(form -> form
                         .loginPage("/login")
                         .usernameParameter("email")
-                        .successHandler((request, response, authentication) -> {
-                            var authorities = authentication.getAuthorities();
-
-                            if (authorities.stream().anyMatch(authority -> authority.getAuthority().equals("ADMIN"))) {
-                                response.sendRedirect("/users");
-                            } else {
-                                String email = authentication.getName();
-                                User currentUser = userRepository.findByEmail(email)
-                                        .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
-                                response.sendRedirect("/users/" + currentUser.getId());
-                            }
-                        })
+                        .successHandler(successHandler)
                 )
-
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login")
