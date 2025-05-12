e2d854ebb9e9f619de0d5461f30f626d7f361609
Naivezz <vykpishdima1@gmail.com>
2025-05-12 13:12:17 +0200

    Added new repository methods

--- service/src/main/java/com/naivez/repository/ReservationRepository.java
+++ service/src/main/java/com/naivez/repository/ReservationRepository.java
@@ -1,7 +1,16 @@
 package com.naivez.repository;
 
 import com.naivez.entity.Reservation;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @EntityGraph(attributePaths = {"restaurant"})
+    @Query("SELECT r FROM Reservation r WHERE r.user.email = :email")
+    List<Reservation> findByUserEmail(@Param("email") String email);
 }
