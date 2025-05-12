7ab07671f1b141169080b715fe69f978f7655b09
Naivezz <vykpishdima1@gmail.com>
2025-05-12 13:02:45 +0200

    Added rolback for each changeset in my changelog file's

--- service/src/main/resources/db.changelog/db.changelog-1.1.sql
+++ service/src/main/resources/db.changelog/db.changelog-1.1.sql
@@ -2,6 +2,9 @@
 
 --changeset naivez:1
 ALTER TABLE users RENAME COLUMN is_blacklisted TO blacklisted;
+--rollback ALTER TABLE users RENAME COLUMN blacklisted TO is_blacklisted;
+
 
 --changeset naivez:2
 ALTER TABLE users ALTER COLUMN blacklisted SET DEFAULT FALSE;
+--rollback ALTER TABLE users ALTER COLUMN blacklisted DROP DEFAULT;
