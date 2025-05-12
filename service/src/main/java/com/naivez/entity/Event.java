d289b64440a2b6d1ecec8474af1bb150ba1b334c
Naivezz <vykpishdima1@gmail.com>
2025-05-12 13:05:00 +0200

    Updated timestamp in entity to LocalDateTime for view layer

--- service/src/main/java/com/naivez/entity/Event.java
+++ service/src/main/java/com/naivez/entity/Event.java
@@ -1,18 +1,17 @@
 package com.naivez.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
 
 @Data
@@ -30,7 +29,7 @@ public class Event implements BaseEntity<Long>{
 
     private String description;
 
-    private Instant time;
+    private LocalDateTime time;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "restaurant_id")
